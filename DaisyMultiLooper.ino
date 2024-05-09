d75f847316d8333122a2313362fa58b54971b556
Colin Love <colinlove@users.noreply.github.com>
2024-05-10 06:22:09 +0930

    increase to 4 loops, spinning animation

--- DaisyMultiLooper.ino
+++ DaisyMultiLooper.ino
@@ -8,10 +8,12 @@
 #define SINGLE_MAX_SIZE (48000 * 20) // 20 seconds of floats at 48 khz
 #define LOOP2OFFSET (SINGLE_MAX_SIZE)
 #define LOOP3OFFSET (SINGLE_MAX_SIZE * 2)
-#define TOTAL_MAX_SIZE (SINGLE_MAX_SIZE * 3) // 3 loops
+#define LOOP4OFFSET (SINGLE_MAX_SIZE * 3)
+#define TOTAL_MAX_SIZE (SINGLE_MAX_SIZE * 4) // 4 loops
 
 Adafruit_AlphaNum4 disp = Adafruit_AlphaNum4();
 int animloop = 0;
+
 
 static DaisyHardware petal;
 
@@ -23,15 +25,13 @@ enum State {
   overdub, // used in all loops
   idle // used in all loops
 };
-State loop1State = idle_empty;
-State loop2State = idle;
-State loop3State = idle;
+State loop1State, loop2State, loop3State, loop4State;
 
 int pos = 0;
 float DSY_SDRAM_BSS buf[TOTAL_MAX_SIZE];
 int mod = SINGLE_MAX_SIZE;
 int len = 0;
-  
+uint16_t time_marker;
 
 void ResetBuffer();
 void Controls();
@@ -62,7 +62,10 @@ void setup() {
 
   petal = DAISY.init(DAISY_PETAL, AUDIO_SR_48K);
   ResetBuffer();
-  loop1State = idle_empty;
+    loop1State = idle_empty;
+    loop2State = idle_empty;
+    loop3State = idle_empty;
+    loop4State = idle_empty;
 
   // start callback
   DAISY.begin(AudioCallback);
@@ -80,7 +83,7 @@ void setup() {
 }
 
 void loop() {
-
+/*
   if (petal.buttons[0].Pressed()) Serial.print("X"); else Serial.print("_");
   if (petal.buttons[1].Pressed()) Serial.print("X"); else Serial.print("_");
   if (petal.buttons[2].Pressed()) Serial.print("X"); else Serial.print("_");
@@ -88,10 +91,12 @@ void loop() {
   if (petal.buttons[4].Pressed()) Serial.print("X"); else Serial.print("_");
   if (petal.buttons[5].Pressed()) Serial.print("X"); else Serial.print("_");
   if (petal.buttons[6].Pressed()) Serial.print("X"); else Serial.print("_");
+  if (petal.buttons[7].Pressed()) Serial.print("X"); else Serial.print("_");
   Serial.println(" <- buttons");
   printStatus(loop1State, 1);
   printStatus(loop2State, 2);
   printStatus(loop3State, 3);
+  printStatus(loop4State, 4);
   Serial.print("mod: ");
   Serial.print(mod);
   Serial.print(", pos: ");
@@ -102,7 +107,7 @@ void loop() {
   digitalWrite(22, (loop1State==armed));
   digitalWrite(1, (loop1State==play));
   digitalWrite(0, ((loop1State == rec_first) || (loop1State == overdub)));
-
+*/
   animloop++;
   if (animloop==4) animloop = 0;
   if (loop1State==idle_empty) {
@@ -137,33 +142,57 @@ void loop() {
     disp.writeDigitRaw(2, 0b1000);
     disp.writeDigitRaw(1, 0b1000);
   } else {
+    if (pos<mod/8) {
+      time_marker = 0b1000000000000;
+    } else if (pos<mod*2/8) {
+      time_marker = 0b1100000000000;
+    } else if (pos<mod*3/8) {
+      time_marker = 0b1100001000000;
+    } else if (pos<mod*4/8) {
+      time_marker = 0b1100101000000;
+    } else if (pos<mod*5/8) {
+      time_marker = 0b1101101000000;
+    } else if (pos<mod*6/8) {
+      time_marker = 0b1111101000000;
+    } else if (pos<mod*7/8) {
+      time_marker = 0b1111111000000;
+    } else {
+      time_marker = 0b11111111000000;
+    } 
     if (loop1State==play) {
-      disp.writeDigitRaw(3, 0b10010000000110);
+      //disp.writeDigitRaw(3, 0b10010000000110);
+      disp.writeDigitRaw(3, time_marker);
     } else if (loop1State==overdub) {
       disp.writeDigitRaw(3, 0b10110100110110);
     } else {
       disp.writeDigitRaw(3, 0b0);
     }
     if (loop2State==play) {
-      disp.writeDigitRaw(2, 0b10010000000110);
+      disp.writeDigitRaw(2, time_marker);
     } else if (loop2State==overdub) {
       disp.writeDigitRaw(2, 0b10110100110110);
     } else {
       disp.writeDigitRaw(2, 0b0);
     }
     if (loop3State==play) {
-      disp.writeDigitRaw(1, 0b10010000000110);
+      disp.writeDigitRaw(1, time_marker);
     } else if (loop3State==overdub) {
       disp.writeDigitRaw(1, 0b10110100110110);
     } else {
       disp.writeDigitRaw(1, 0b0);
     }
-    disp.writeDigitRaw(0, 0b0);
+    if (loop4State==play) {
+      disp.writeDigitRaw(0, time_marker);
+    } else if (loop4State==overdub) {
+      disp.writeDigitRaw(0, 0b10110100110110);
+    } else {
+      disp.writeDigitRaw(0, 0b0);
+    }
   }
 
   disp.writeDisplay();
 
-  delay(200);
+  delay(100);
 }
 
 void printStatus(State state, int name) {
@@ -222,9 +251,12 @@ void UpdateButtons() {
     }
     if (loop2State == overdub) loop2State = play; // downgrade any recording loops to playing
     if (loop3State == overdub) loop3State = play;
+    if (loop4State == overdub) loop4State = play;
   }
   if (petal.buttons[3].RisingEdge()) { // loop 2 rec
     switch (loop2State) {
+      case idle_empty:
+        if (loop1State==idle_empty || loop1State==armed) break;
       case idle:
       case play:
         loop2State = overdub;
@@ -233,11 +265,19 @@ void UpdateButtons() {
         loop2State = play;
         break;
     }
+    if (loop1State == rec_first) {
+      mod = len;
+      len = 0;
+      loop1State = play;
+    }
     if (loop1State == overdub) loop1State = play; // downgrade any recording loops to playing
     if (loop3State == overdub) loop3State = play;
+    if (loop4State == overdub) loop4State = play;
   }
   if (petal.buttons[5].RisingEdge()) { // loop 3 rec
     switch (loop3State) {
+      case idle_empty:
+        if (loop1State==idle_empty || loop1State==armed) break;
       case idle:
       case play:
         loop3State = overdub;
@@ -246,8 +286,35 @@ void UpdateButtons() {
         loop3State = play;
         break;
     }
+    if (loop1State == rec_first) {
+      mod = len;
+      len = 0;
+      loop1State = play;
+    }
     if (loop1State == overdub) loop1State = play; // downgrade any recording loops to playing
     if (loop2State == overdub) loop2State = play;
+    if (loop4State == overdub) loop4State = play;
+  }
+  if (petal.buttons[7].RisingEdge()) { // loop 4 rec
+    switch (loop4State) {
+      case idle_empty:
+        if (loop1State==idle_empty || loop1State==armed) break;
+      case idle:
+      case play:
+        loop4State = overdub;
+        break;
+      case overdub:
+        loop4State = play;
+        break;
+    }
+    if (loop1State == rec_first) {
+      mod = len;
+      len = 0;
+      loop1State = play;
+    }
+    if (loop1State == overdub) loop1State = play; // downgrade any recording loops to playing
+    if (loop2State == overdub) loop2State = play;
+    if (loop3State == overdub) loop3State = play;
   }
   if (petal.buttons[0].RisingEdge()) { // pressed loop 1 play button
     switch (loop1State) {
@@ -277,7 +344,7 @@ void UpdateButtons() {
     }
   }
   if (petal.buttons[4].RisingEdge()) { // pressed loop 3 play button
-    switch (loop2State) {
+    switch (loop3State) {
       case overdub:
       case idle:
         loop3State = play;
@@ -287,11 +354,23 @@ void UpdateButtons() {
         break;
     }
   }
+  if (petal.buttons[6].RisingEdge()) { // pressed loop 4 play button
+    switch (loop4State) {
+      case overdub:
+      case idle:
+        loop4State = play;
+        break;
+      case play:
+        loop4State = idle;
+        break;
+    }
+  }
   if (petal.buttons[0].TimeHeldMs() >= 1000 && loop1State != idle_empty) { // hold play button
     ResetBuffer();
     loop1State = idle_empty;
-    loop2State = idle;
-    loop3State = idle;
+    loop2State = idle_empty;
+    loop3State = idle_empty;
+    loop4State = idle_empty;
   }
   if (petal.buttons[0].Pressed() && petal.buttons[1].Pressed()) {
     for (int i = 0; i < LOOP2OFFSET; i++) buf[i] = 0;
@@ -302,10 +381,17 @@ void UpdateButtons() {
     loop2State = idle;
   }
   if (petal.buttons[4].Pressed() && petal.buttons[5].Pressed()) {
-    for (int i = LOOP3OFFSET; i < TOTAL_MAX_SIZE; i++) buf[i] = 0;
+    for (int i = LOOP3OFFSET; i < LOOP4OFFSET; i++) buf[i] = 0;
     loop3State = idle;
   }  
-  if ((loop1State == idle) && (loop2State == idle) && (loop3State == idle)) pos = 0; // if nothing else is playing, move playback head to start
+  if (petal.buttons[6].Pressed() && petal.buttons[7].Pressed()) {
+    for (int i = LOOP4OFFSET; i < TOTAL_MAX_SIZE; i++) buf[i] = 0;
+    loop4State = idle;
+  }  
+  if ((loop1State == idle || loop1State == idle_empty) && 
+      (loop2State == idle || loop2State == idle_empty) && 
+      (loop3State == idle || loop3State == idle_empty) && 
+      (loop4State == idle || loop4State == idle_empty)) pos = 0; // if nothing else is playing, move playback head to start
 }
 
 void NextSamples(float &output, float *in, size_t i) {
@@ -315,10 +401,12 @@ void NextSamples(float &output, float *in, size_t i) {
   if (loop1State!=idle) output += buf[pos];
   if (loop2State!=idle) output += buf[LOOP2OFFSET + pos];
   if (loop3State!=idle) output += buf[LOOP3OFFSET + pos];
+  if (loop4State!=idle) output += buf[LOOP4OFFSET + pos];
   // record new layers if recording active for that looper
   if ((loop1State == rec_first) || (loop1State == overdub)) buf[pos] += in[i];
   if (loop2State == overdub) buf[LOOP2OFFSET + pos] += in[i];
   if (loop3State == overdub) buf[LOOP3OFFSET + pos] += in[i];
+  if (loop4State == overdub) buf[LOOP4OFFSET + pos] += in[i];
   // increment looptime if this is the first layer
   if (loop1State == rec_first) len++;
 
@@ -331,7 +419,8 @@ void NextSamples(float &output, float *in, size_t i) {
   // if anything is playing or recording then increment position
   if ((loop1State == rec_first) || (loop1State == play) || (loop1State == overdub)
         || (loop2State == play) || (loop2State == overdub)
-        || (loop3State == play) || (loop3State == overdub)) {
+        || (loop3State == play) || (loop3State == overdub)
+        || (loop4State == play) || (loop4State == overdub)) {
     pos++;
     pos %= mod;
   }
